c56913ef943e9a1beae4bd49cdec14cb17a6b70d
izelbaltazar <723526@student.dusd.net>
2022-12-01 09:56:26 -0800

    Today I coded and created Health collectibles.
    Today I coded Ruby's health and created health collectibles.

--- RubysAdventureBaltazar/Assets/Scripts/RubyController.cs
+++ RubysAdventureBaltazar/Assets/Scripts/RubyController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    
     public int maxHealth = 5;
-    int currentHealth; 
+
+    public int health { get { return currentHealth; }}
+    public int currentHealth;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -14,10 +18,9 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
+        
     }
-
 
     // Update is called once per frame
     void Update()
@@ -34,9 +37,9 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth (int amount)
+    public void ChangeHealth(int amount)
     {
-        currentHHealth = Mathf. Clamp
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
-
-
+}
